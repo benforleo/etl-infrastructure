5fb616ecc74f20107d7c0af1a76c6b23eb8b9037
Ben Forleo <40501242+benforleo@users.noreply.github.com>
2021-02-07 15:37:25 -0500

    still getting multi-part upload error

--- create.sh
+++ create.sh
@@ -1,1 +1,3 @@
-aws cloudformation create-stack --stack-name myteststack --template-body file://s3.yml --capabilities CAPABILITY_IAM
+#aws cloudformation create-stack --stack-name myteststack --template-body file://s3.yml --capabilities CAPABILITY_IAM
+
+#aws cloudformation update-stack --stack-name myteststack --template-body file://s3.yml --capabilities CAPABILITY_IAM
